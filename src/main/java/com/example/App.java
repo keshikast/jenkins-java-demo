ff893ed43163269ceec9ea18195ae42182632f7e
Keshika Subhashini Thirumurugan <keshikathirums@gmail.com>
2026-08-19 14:13:47 +0530

    Fix typo in greeting message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static String getGreeting() {
-        return "Hello, Jenkins Java CI/CD Pipeline!\npoll scm trigger jenkins";
+        return "Hello, Jenkins Java CI/CD Pipeline!\nPoll SCM trigger jenkins";
     }
 
     public static void main(String[] args) {
