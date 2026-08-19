34b51b8686d2fbf3c690d185288f01aa693733c4
Keshika Subhashini Thirumurugan <keshikathirums@gmail.com>
2026-08-19 14:11:45 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static String getGreeting() {
-        return "Hello, Jenkins Java CI/CD Pipeline!";
+        return "Hello, Jenkins Java CI/CD Pipeline!\npoll scm trigger jenkins";
     }
 
     public static void main(String[] args) {
