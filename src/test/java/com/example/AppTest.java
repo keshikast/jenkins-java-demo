abd0f8255e9945e93fce009dc601257a496738d5
Keshika Subhashini Thirumurugan <keshikathirums@gmail.com>
2026-08-19 14:12:27 +0530

    Update testGreeting to check for new greeting message

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -6,6 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void testGreeting() {
-        assertEquals("Hello, Jenkins Java CI/CD Pipeline!", App.getGreeting());
+        assertEquals("Hello, Jenkins Java CI/CD Pipeline!\npoll scm trigger jenkins", App.getGreeting());
     }
 }
