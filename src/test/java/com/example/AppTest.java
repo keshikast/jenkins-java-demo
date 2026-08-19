2c2d13526c1f1b88b07ae8fc5c7925082af89ea5
Keshika Subhashini Thirumurugan <keshikathirums@gmail.com>
2026-08-19 14:43:49 +0530

    Update greeting message in AppTest

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -6,6 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void testGreeting() {
-        assertEquals("Hello, Jenkins Java CI/CD Pipeline!\nPoll SCM trigger jenkins\nshow git update", App.getGreeting());
+        assertEquals("Hello, Real-time Webhook Trigger!", App.getGreeting());
     }
 }
