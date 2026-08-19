0b1c394bf7b58fcd47659787ca955840a45841ce
Keshika Subhashini Thirumurugan <keshikathirums@gmail.com>
2026-08-19 13:51:46 +0530

    Add App class with greeting method and main method

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static String getGreeting() {
+        return "Hello, Jenkins Java CI/CD Pipeline!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+    }
+}
